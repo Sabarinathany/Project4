ed3cb76df50e089a3c899306608a5cb2e5036a94
Sabarinathany <sabarinathany22@gmail.com>
2024-06-11 18:17:06 +0530

    first commit

---
+++ TempFile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../Tenplate/TempFile.css";
+const TempFile = () => {
+  return (
+    <div>
+      <p id="a">hey</p>
+      <p id="b">my Love !</p>
+    </div>
+  );
+};
+export default TempFile;
